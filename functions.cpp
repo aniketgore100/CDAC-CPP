3ef4f24cdee2d962b3402e37116a1a8bf3909117
Aniket <goreaniket100@gmail.com>
2023-09-29 12:48:52 +0530

    functions changed

--- functions.cpp
+++ functions.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 // pass by value 
 
-void welcome(string naam)//body--code
-{
-	cout<<"\n1.Welcome to CDAC: "<<naam<<" address:"<<&naam;
-	naam="DR."+naam;
-	cout<<"\n2.Welcome to CDAC: "<<naam<<" address:"<<&naam;
-}
+// void welcome(string naam)//body--code
+// {
+// 	cout<<"\n1.Welcome to CDAC: "<<naam<<" address:"<<&naam;
+// 	naam="DR."+naam;
+// 	cout<<"\n2.Welcome to CDAC: "<<naam<<" address:"<<&naam;
+// }
 
-// pass my reference
-void welcome2(string &naam)//body--code refence &
-{
-	cout<<"\n1.Welcome to CDAC: "<<naam<<" address is:"<<&naam;
-	naam="DR."+naam;
-	cout<<"\n2.Welcome to CDAC: "<<naam<<" address is:"<<&naam;
+// // pass my reference
+// void welcome2(string &naam)//body--code refence &
+// {
+// 	cout<<"\n1.Welcome to CDAC: "<<naam<<" address is:"<<&naam;
+// 	naam="DR."+naam;
+// 	cout<<"\n2.Welcome to CDAC: "<<naam<<" address is:"<<&naam;
+// }
+void sp(int &a, int &b){
+    // a = a^b;
+    // b = a^b;
+    // a = a^b;
+    swap(a, b);
+    cout<<"after swap in fuction"<<a <<" "<<b<<endl;
 }
 
 int main()
 {
-	cout<<"Enter your name:";
-	string name;
-	cin>>name;
-	welcome(name);
-    welcome2(name);
-    cout<<"\n:After call"<<name<<" address:"<<&name;
+    int a, b;
+    cin>>a>>b;
+    sp(a,b);
+     cout<<"after swap in fuction"<<a <<" "<<b<<endl;
 }
 
