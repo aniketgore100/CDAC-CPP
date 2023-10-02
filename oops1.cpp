b88df3110eb2405243ec223851b3b00cb580c98c
Aniket <goreaniket100@gmail.com>
2023-10-02 10:41:46 +0530

    virtual absract class

--- oops1.cpp
+++ oops1.cpp
@@ -141,49 +141,104 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+// class Human
+// { private:
+// 	string name;
+//   public:
+//   	Human(string n)
+//   	{
+//   		cout<<"\nHi i m constructor from human";
+//   		name=n;
+//   		cout<<"\nName set to: "<<name<<" by constructor:Human";
+// 	}
+// };
+
+// class Education:public Human
+// {             
+// 	private:
+// 		string degree;
+
+//   public:
+//   	Education(string n,string d):Human(n)
+//   	{
+//   		cout<<"\nHi i m constructor from Education";
+//   		degree=d;
+//   		cout<<"\nDegree set to: "<<degree<<" by constructor:Education";
+// 	}
+// };
+
+// class Job:public Education
+// {
+// 	private:
+// 		string job;
+// 	public:
+// 	   //contructor(parameters):constructor_super_class(parameters)
+// 	   Job(string n,string d,string j):Education(n,d)
+// 	   {
+// 	   	cout<<"\nHi i m constructor from Job";
+//   		job=j;
+//   		cout<<"\nJob set to: "<<job<<" by constructor:Job";
+// 	   }
+
+// };
+// int main()
+// {
+// 	Job j("Amar","Phd","Trainer");
+// }
+
+
 #include <iostream>
 using namespace std;
-class Human
-{ private:
-	string name;
-  public:
-  	Human(string n)
-  	{
-  		cout<<"\nHi i m constructor from human";
-  		name=n;
-  		cout<<"\nName set to: "<<name<<" by constructor:Human";
-	}
+
+class shape{
+    public:
+    virtual void printShapeArea() = 0;
 };
 
-class Education:public Human
+class circle : public shape{
+    private:
+    float r;
+
+    public:
+    void setRadius(float r){
+        this->r = r;
+    }
+
+    void printShapeArea()
+    {
+        cout << "Circle Area: " << 3.14 * r * r << endl;
+    }
+};
+
+class rectangle : public shape
 {
-	private:
-		string degree;
+private:
+    float l, b;
 
-  public:
-  	Education(string n,string d):Human(n)
-  	{
-  		cout<<"\nHi i m constructor from Education";
-  		degree=d;
-  		cout<<"\nDegree set to: "<<degree<<" by constructor:Education";
-	}
+public:
+    void setDimensions(float l, float b)
+    {
+        this->l = l;
+        this->b = b;
+    }
+
+    void printShapeArea()
+    {
+        cout << "Rectangle Area: " << this->l * this->b << endl;
+    }
 };
 
-class Job:public Education
-{
-	private:
-		string job;
-	public:
-	   //contructor(parameters):constructor_super_class(parameters)
-	   Job(string n,string d,string j):Education(n,d)
-	   {
-	   	cout<<"\nHi i m constructor from Job";
-  		job=j;
-  		cout<<"\nJob set to: "<<job<<" by constructor:Job";
-	   }
-
-};
 int main()
 {
-	Job j("Amar","Phd","Trainer");
+    circle c;
+    c.setRadius(3);
+    c.printShapeArea();
+
+    rectangle r;
+    r.setDimensions(3, 4);
+    r.printShapeArea();
+
+    return 0;
 }
