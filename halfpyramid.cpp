44627ad5eae7923afb1e2558ba734355e6033166
Aniket <goreaniket100@gmail.com>
2023-10-06 10:34:52 +0530

    patterns problems added

--- halfpyramid.cpp
+++ halfpyramid.cpp
@@ -2,115 +2,94 @@
 // using namespace std;
 
 // int main(){
+//   int n;
+//   cin>>n;
+
+//   // }
+//   for(int i = 1; i<=n; i++){
+//     for(int k = 1; k<=n-i; k++){
+//       cout<<" ";
+//     }
+//     for(int j = 1; j<=2*i-1; j++){
+//         if (j == 1 || j == 2 * i - 1 || i == n) {
+//                cout << "*";
+//             } else {
+//               cout << " ";
+//             }
+//     }
+    
+//     cout<<endl;
+//   }
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   for(int i = 1; i<=n; i++){
+//     for(int k = 1; k <= n-i; k++){
+//       cout<<" ";
+//     }
+//     for(int j = 1; j<=2*i-1; j++){
+//       cout<<"*";
+//     }
+//     cout<<endl;
+//   }
+
+
+//   for(int i = n-1; i>=1; i--){
+//     for(int k = 1; k <= n-i; k++){
+//       cout<<" ";
+//     }
+//     for(int j = 1; j<=2*i-1; j++){
+//       cout<<"*";
+//     }
+//     cout<<endl;
+//   }
+// }
+
+//      0
+//     00
+//    000
+//   0000
+//  00000
+// 000000
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
 //     int n;
 //     cin>>n;
 //     for(int i = 1; i<=n; i++){
-//         for(int j = 1; j<=i; j++){
-//             cout<<"*";
+//         for(int j = 1; j<=n; j++){
+//             if((i==1) || (j == 1) || (i+j == n+1)){
+//                 cout<<"*";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
 //         }
 //         cout<<endl;
 //     }
 // }
 
 
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cin>>n;
-//     for(int i = 1; i<=n; i++){
-//         for(int j = 1; j<=n-i+1; j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// }
-
-
 
 // #include <bits/stdc++.h>
 // using namespace std;
 
 // int main(){
-//     int n;
-//     cin>>n;
-//     for(int i = 1; i<=n; i++){
-//        for(int k = n-i; k>0; k--){
-//         cout<<" ";
-//        }
-//         for(int j = 1; j<=i; j++){
-//             cout<<"*";
-//        }
-//         cout<<endl;
+//   int n;
+//   cin>>n;
+//   char c = 'a';
+//    for(char i = 1; i<=n; i++){
+//     for(char j = 1; j<=i; j++){
+//       cout<<c++;
 //     }
-// }
-
-
-
-
-
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cin>>n;
-//     for(int i = 1; i<=n; i++){
-//        for(int k = n-i; k>0; k--){
-//         cout<<" ";
-//        }
-//         for(int j = 1; j<=i; j++){
-//             cout<<"*";
-//        }
-//         cout<<endl;
-//     }
-// }
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    for(int i = 1; i<=n; i++){
-      for(int k = n-i; k>0; k--){
-        cout<<" ";
-      }
-      for(int j = 1; j<=i; j++){
-        cout<<"* ";
-      }
-      cout<<endl;
-    }
-     for(int i = n; i>=1; i--){
-      for(int k = n-i; k>0; k--){
-        cout<<" ";
-      }
-      for(int j = i; j>0; j--){
-        cout<<"* ";
-      }
-      cout<<endl;
-    }
-}
-
-
-
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cin>>n;
-//     for(int i = n; i>=1; i--){
-//       for(int k = n-i; k>0; k--){
-//         cout<<" ";
-//       }
-//       for(int j = i; j>0; j--){
-//         cout<<"* ";
-//       }
-//       cout<<endl;
-//     }
+//     cout<<endl;
+//    }
 // }
